3514d23c27c19cf507ec0da5a8edeeae5ac00334
Christopher Douglass <christopher.d.douglass@gmail.com>
2019-08-10 17:04:32 -0700

    discovered that the page source was too cumbersome to deal with, so I've decided to create the html myself

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile('home.html', {root: path.join(__dirname, './HtmlFiles')});
 });
 
+app.get('/plated-desserts', (req, res) => {
+  res.sendFile('plated-desserts.html', {root: path.join(__dirname, './HtmlFiles')});
+});
+
 if (module === require.main) {
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
