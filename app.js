6f72dd2935b6379aadb2255d658b7ec14db16650
Christopher Douglass <christopher.d.douglass@gmail.com>
2019-08-12 11:55:29 -0700

    linked stylesheets so they could be accessed when running the app through node

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile('home.html', {root: path.join(__dirname, './HtmlFiles')});
 });
