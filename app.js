a2bbb2c1147083bb095198abbbdc39dde337921b
Christopher Douglass <christopher.d.douglass@gmail.com>
2019-08-12 16:26:08 -0700

    removed connect.js and consolidated code into app.js

--- app.js
+++ app.js
@@ -3,21 +3,138 @@
 // Imports
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const ENV = require('dotenv');
+ENV.config();
 
-// Init Routes
+// Bring in models
+let User = require('./models/user');
+
+// Passport config
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      console.log("Verification function called");
+      if (err) { return done(err); }
+      console.log("No err");
+      if (!user) { 
+          console.log("No user");
+          return done(null, false, { message: 'No user found' }); 
+      }
+      
+      bcrypt.compare(password, user.password, function(err, isMatch) {
+          if(err) throw err;
+          console.log("No pass error");
+
+          if(isMatch) {
+              console.log("Pass match");
+              return done(null, user);
+          } else {
+              console.log("Pass no match");
+              return done(null, false); 
+          }
+      });
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(err, user);
+});
+
+// Init App
 const app = express();
 
 // Link static files
 app.use(express.static('public'));
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect to mongodb
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+// Connect to db and check for errors
+db.once('open', () => {
+  console.log('Connected');
+});
+
+db.on('error', (err) => {
+  console.log(err);
+});
 
 // Add Routes
 app.get('/', (req, res) => {
   res.sendFile('home.html', {root: path.join(__dirname, './HtmlFiles')});
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile('register.html', {root: path.join(__dirname, './HtmlFiles')});
+});
+
+app.post('/register', [
+      check('email')
+          .isEmail().withMessage('Must be a valid email'),
+      check('password')
+          .isLength({ min: 5 }).withMessage('Must be at least 5 chars long')
+          .custom((val, {req, loc, path}) => {
+              if(val !== req.body.password2) {
+                  throw new Error('Passwords don\'t match');
+              } else {
+                  return val;
+              }
+          }).withMessage('Passwords must match')
+], (req, res) => {    
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+  } else {
+      let newUser = new User();
+
+      newUser.username = req.body.email;
+      newUser.password = req.body.password;
+
+    bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) {
+                  console.log(err);
+              }
+              newUser.password = hash;
+              newUser.save((err) => {
+                  if(err){
+                      console.log(err);
+                  } else {
+                      res.redirect('/login');
+                  }
+              });
+          });
+      });
+    }
+});
+
 app.get('/login', (req, res) => {
   res.sendFile('sign-in.html', {root: path.join(__dirname, './HtmlFiles')});
+});
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+     successRedirect: '/', failureRedirect: '/login' 
+  })(req, res, next);
 });
 
 app.get('/about', (req, res) => {
